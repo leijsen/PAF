1aa881a75a8fe9ad50930a522b70787f833487d7
leijsen <2198510562@qq.com>
2023-01-18 19:28:37 +0800

    didi

--- github.py
+++ github.py
@@ -3,3 +3,8 @@
 # git commit -m "第一次提交" //注释说明
 # git remote add origin https://github.com/***/VSCode_Git_Test.git （这里是你的github仓库地址）
 # git push -u origin master  //提交到你的仓库
+
+# 关闭origin操作
+# 1、先输入git remote rm origin 删除关联的origin的远程库
+# 2、关联自己的仓库 git remote add origin https://gitee.com/xxxxxx.git
+# 3、最后git push origin master，这样就推送到自己的仓库了。
