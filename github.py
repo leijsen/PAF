3b54371c7eb43edbdf1921b6b1c5c0f715e12aac
leijsen <86351229+leijsen@users.noreply.github.com>
2023-01-18 19:41:59 +0800

    Update github.py

--- github.py
+++ github.py
@@ -8,3 +8,9 @@
 # 1、先输入git remote rm origin 删除关联的origin的远程库
 # 2、关联自己的仓库 git remote add origin https://gitee.com/xxxxxx.git
 # 3、最后git push origin master，这样就推送到自己的仓库了。
+
+# 代理解决方案
+# //取消http代理
+# git config --global --unset http.proxy
+# //取消https代理 
+# git config --global --unset https.proxy
