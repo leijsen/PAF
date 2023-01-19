0ed159938e77f435e8b46db60d0dd3b5c6cddf35
leijsen <2198510562@qq.com>
2023-01-19 16:13:13 +0800

    ok

--- github.py
+++ github.py
@@ -2,6 +2,7 @@
 # git add test.php（随便一个你的文件）
 # git commit -m "第一次提交" //注释说明
 # git remote add origin https://github.com/***/VSCode_Git_Test.git （这里是你的github仓库地址）
+## git pull origin master
 # git push -u origin master  //提交到你的仓库
 
 # 关闭origin操作
